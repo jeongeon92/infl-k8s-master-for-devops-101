6e5a7ad293b4919c46021c7a631e290c858864a1
JE.KIM <jeongeon92@outlook.com>
2022-05-01 15:21:50 +0900

    test_server.py updated

--- 02 Docker Basic Lectures/echo_test/test_server.py
+++ 02 Docker Basic Lectures/echo_test/test_server.py
@@ -5,6 +5,7 @@ with socket.socket() as s:
   s.bind(("0.0.0.0", 12345))
   s.listen()
   print("server is started")
+  
   conn, addr = s.accept()
   # conn 클라이언트와 통신할 소켓
   # addr 클라이언트의 정보가 들어있음
@@ -14,4 +15,3 @@ with socket.socket() as s:
       data = conn.recv(1024)
       if not data: break
       conn.sendall(data)
-      
